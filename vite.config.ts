3782a548c03744c0b6b399d530b0ce26da9385f0
planethouki <26595148+planethouki@users.noreply.github.com>
2026-07-12 21:11:48 +0900

    Viteサーバーの設定を修正、CORSとポートの指定

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,21 @@ export default defineConfig({
         react(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'http://localhost:5173',
+        cors: {
+            origin: [
+                'http://localhost:8000',
+                'http://127.0.0.1:8000',
+                'http://0.0.0.0:8000',
+            ],
+        },
+        hmr: {
+            host: 'localhost',
+            clientPort: 5173,
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
